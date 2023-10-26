c8c9c01ae3e75fddab079ef22cb2e204342c8f57
hungryx <yexin121101@gmail.com>
2023-10-26 17:16:20 +0800

    search button for querying registered domains

--- frontend/src/routes/domain/DomainList.tsx
+++ frontend/src/routes/domain/DomainList.tsx
@@ -1,52 +1,87 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 import Header from "../../components/header/Header";
 import DomainPanel from "../../components/panels/DomainsPanel";
-import {Domain, dnsContract} from "../../api/dns/dns";
-import {useWallet} from "../../api/wallet/wallet";
+import { Domain, dnsContract } from "../../api/dns/dns";
+import { useWallet } from "../../api/wallet/wallet";
 
-import {Box, Grid, Typography,} from "@mui/material";
-import {WithLoader} from "../../components/hoc/hoc";
+import { Box, Grid, Typography } from "@mui/material";
+import { WithLoader } from "../../components/hoc/hoc";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 import style from "./DomainCommon.module.css";
 
 export default function DomainList() {
-    const {provider, signer} = useWallet();
-    const [domains, setDomains] = React.useState<Domain[]>([]);
-    const [loading, setLoading] = React.useState(true);
+  const { provider, signer } = useWallet();
+  const [domains, setDomains] = React.useState<Domain[]>([]);
+  const [domainList, setDomainsList] = React.useState(Array);
+  const [domainsFiltered, setDomainsFiltered] = React.useState<Domain[]>([]);
+  const [loading, setLoading] = React.useState(true);
 
-    useEffect(() => {
-        getOwnersDomains();
-    }, []);
+  useEffect(() => {
+    getOwnersDomains();
+  }, []);
 
-    const getOwnersDomains = async () => {
-        const domains = await dnsContract.getAllDomainRegistered(provider);
-        setDomains(domains);
-        setLoading(false);
+  const getOwnersDomains = async () => {
+    const domains = await dnsContract.getAllDomainRegistered(provider);
+    setDomains(domains);
+    setDomainsFiltered(domains);
+    setDomainsList(domains.map((d) => d.name));
+    setLoading(false);
+  };
+
+  const onChange = (data: any[], query: string) => {
+    if (!query) {
+      setDomainsFiltered(data);
     }
-
-    return (
-        <>
-            <Header/>
-            <Grid container className={style.content} alignContent={"center"} alignItems="center">
-                <Grid item xs={12}>
-                    <Box className={style.wrapper}>
-                        <Typography variant={"h5"} fontWeight={"900"}>
-                            All Registered Domains
-                        </Typography>
-                        <Box
-                            className={style.panel}
-                            display={"flex"}
-                            flexDirection={"column"}
-                            justifyContent={"center"}
-                            alignItems={"center"}
-                        >
-                            <WithLoader loading={loading}>
-                                <DomainPanel domains={domains} showOwner/>
-                            </WithLoader>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
+    setDomainsFiltered(
+      data.filter((d) => d.name.toLowerCase().includes(query))
     );
+  };
+
+  return (
+    <>
+      <Header />
+      <Grid
+        container
+        className={style.content}
+        alignContent={"center"}
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <Box className={style.wrapper}>
+            <Typography variant={"h5"} fontWeight={"900"}>
+              All Registered Domains
+            </Typography>
+
+            <Autocomplete
+              className={style.search}
+              id="free-solo-demo"
+              options={domainList}
+              renderInput={(params) => (
+                <TextField
+                  className={style.search_input}
+                  sx={{ "& fieldset": { border: "none" } }} // Removes border
+                  {...params}
+                  placeholder={"Search for a registered domain"}
+                  onChange={(e) => onChange(domains, e.target.value)}
+                />
+              )}
+            />
+            <Box
+              className={style.panel}
+              display={"flex"}
+              flexDirection={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <WithLoader loading={loading}>
+                <DomainPanel domains={domainsFiltered} showOwner />
+              </WithLoader>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
